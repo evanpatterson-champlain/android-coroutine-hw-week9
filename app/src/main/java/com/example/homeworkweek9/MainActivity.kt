34a1322a8430fd57344e961d474ea07704202c64
evan patterson <thedeepdaemon@gmail.com>
2022-04-24 00:04:09 -0400

    Cancels image requests

--- app/src/main/java/com/example/homeworkweek9/MainActivity.kt
+++ app/src/main/java/com/example/homeworkweek9/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.homeworkweek9
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
@@ -8,20 +10,20 @@ import android.view.View
 import android.view.View.OnTouchListener
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homeworkweek9.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-
-
+import kotlinx.coroutines.*
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+import kotlin.coroutines.coroutineContext
 
 
 private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
-    private val imageStorage = ImageStorage()
     private lateinit var imageURLs: Array<out String>
     private lateinit var imageDescriptions: Array<out String>
     private var curImageIndex = 0
+    private var imgJob: Job? = null
 
     private fun initVariables() {
         imageURLs = resources.getStringArray(R.array.image_url_list)
@@ -43,14 +45,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun launchImageGet() {
-        GlobalScope.launch(Dispatchers.IO) {
-            val imageUrl = imageURLs[curImageIndex]
-            val bitmapImage = imageStorage.requestBitmap(imageUrl)
+        imgJob = GlobalScope.launch(Dispatchers.IO) {
+            val url = URL(imageURLs[curImageIndex])
+            val httpConnection = url.openConnection() as HttpURLConnection
+            httpConnection.doInput = true
+            httpConnection.connect()
+            val inputStream = httpConnection.inputStream
+            val bitmapImage = BitmapFactory.decodeStream(inputStream)
 
             launch(Dispatchers.Main) {
-                Log.d("CoroutineExercise", "Name Of thread is  ${Thread.currentThread().name}")
                 binding.imageView.setImageBitmap(bitmapImage)
                 binding.imageDescription.text = imageDescriptions[curImageIndex]
+                imgJob = null
             }
         }
     }
@@ -59,6 +65,11 @@ class MainActivity : AppCompatActivity() {
     private fun setOnTouchDescription() {
         binding.imageDescription.setOnTouchListener(OnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_DOWN) {
+                if (imgJob != null) {
+                    Log.d("Log", "Cancelled image request")
+                    imgJob!!.cancel("Called a new request")
+                    imgJob = null
+                }
                 curImageIndex++
                 curImageIndex %= imageURLs.size
                 launchImageGet()
